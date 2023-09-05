4d04beb7f86f1f8cf4de257be3af763d9846b6ca
Arkistos <pierre.laco@hotmail.fr>
2023-09-05 14:48:25 +0200

    build: Product, User and Client fixtures creation

---
+++ src/DataFixtures/ClientFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Client;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class ClientFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $client = new Client();
+        $client->setEmail('user@client.com');
+        $hashedPassword = $this->passwordHasher->hashPassword($client, 'password');
+        $client->setPassword($hashedPassword);
+        $this->addReference('client', $client);
+
+        $manager->persist($client);
+
+        $manager->flush();
+    }
+
+
+}
--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -10,9 +10,32 @@ class ProductFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $manager->persist($this->createProduct(
+            'Iphone 14',
+            'Apple',
+            1445.34,
+            '14ème génération d\'iphone',
+        ));
+
+        $manager->persist($this->createProduct(
+            'Galaxy 14',
+            'Samsung',
+            1423,
+            '14ème génération de smartphone Samsung',
+        ));
 
         $manager->flush();
+    }
+
+    private function createProduct(string $name, string $brand, float $price, string $description): Product
+    {
+        $product = new Product();
+        $product->setName($name);
+        $product->setBrand($brand);
+        $product->setPrice($price);
+        $product->setDescription($description);
+        $product->setCreatedAt(new \DateTimeImmutable());
+
+        return $product;
     }
 }
