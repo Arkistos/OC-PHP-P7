0b718ae9ed24b7f30fb795723fd953e491e9fbb3
Arkistos <pierre.laco@hotmail.fr>
2023-09-06 11:59:30 +0200

    feat: route /products/{id}

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,38 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use JMS\Serializer\SerializerInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class ProductController extends AbstractController
 {
-    #[Route('/api/products', name: 'app_product', methods:['GET'])]
+    #[Route('/api/products', name: 'products', methods: ['GET'])]
     public function getProducts(
-        ProductRepository $productRepository, 
+        ProductRepository $productRepository,
         SerializerInterface $serializer,
         Request $request,
         TagAwareCacheInterface $cache
-        ): JsonResponse
-    {
+    ): JsonResponse {
         $page = $request->get('page', 1);
         $limit = $request->get('limit', 5);
-        $cacheId = "getProducts-".$page."-".$limit;
-        $products = $cache->get($cacheId, function(ItemInterface $item) use ($productRepository, $page, $limit){
+        $cacheId = 'getProducts-'.$page.'-'.$limit;
+        $products = $cache->get($cacheId, function (ItemInterface $item) use ($productRepository, $page, $limit) {
             $item->expiresAfter(900);
-            $item->tag("productsCache");
+            $item->tag('productsCache');
+
             return $productRepository->findAllPaginated($page, $limit);
         });
 
+        $jsonProducts = $serializer->serialize($products, 'json');
 
-
-        $jsonProducts = $serializer->serialize($products,'json');
         return new JsonResponse($jsonProducts, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/products/{id}', name: 'products_details', methods: ['GET'])]
+    public function getProduct(Product $product, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonProduct = $serializer->serialize($product, 'json');
+
+        return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
     }
 }
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -8,7 +8,15 @@ use Doctrine\ORM\Mapping as ORM;
 use Hateoas\Configuration\Annotation as Hateoas;
 
 
-
+/**
+ * @Hateoas\Relation(
+ *     "self",
+ *     href = @Hateoas\Route(
+ *         "products_details",
+ *          parameters = {"id" = "expr(object.getId())"}     
+ *      )
+ * )
+ */
 
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
